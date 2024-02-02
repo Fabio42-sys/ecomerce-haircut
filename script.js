426a17e92584fb2d4a74950274ab3eb7e52a70e9
Fábio Luiz <142447343+Fabio42-sys@users.noreply.github.com>
2024-02-02 22:39:01 +0000

    js: preparando a troca de tema e criação das variáveis

--- script.js
+++ script.js
@@ -21,3 +21,85 @@ document.getElementById('toggle-tema').addEventListener('click', () => {
         tema.classList.add('troca-invisivel');
     }
 });
+
+
+/* icones inicio */
+
+let header = document.getElementById('header');
+let menu = document.getElementById('openMenu');
+let cog = document.getElementById('cog');
+let aside = document.getElementById('menuAside');
+let user = document.getElementById('user');
+let home = document.getElementById('home');
+let bell = document.getElementById('bell');
+let date = document.getElementById('calendar');
+let cart = document.getElementById('cart');
+let theme = document.getElementById('switch');
+let moon = document.getElementById('tema-escuro');
+let sun = document.getElementById('tema-claro');
+let angle = document.getElementById('angle');
+
+/* icones fim */ 
+
+/* imgs inicio */
+
+let img1 = document.getElementById('img1');
+let img2 = document.getElementById('img2');
+let img3 = document.getElementById('img3');
+
+/* imgs fim */
+
+/* links inicio */
+
+let config = document.getElementById('config');
+let perfil = document.getElementById('perfil');
+let casa = document.getElementById('casa');
+let paravoce = document.getElementById('4you');
+let agnd = document.getElementById('agenda');
+let package = document.getElementById('package');
+let troca = document.getElementById('tema-switch');
+let check = document.getElementById('agnd');
+let cabelo = document.getElementById('hair');
+let barba = document.getElementById('bear');
+let sombrancelha = document.getElementById('eyebow');
+
+/* links fim */
+
+/* importante inicio */
+
+let comod1 = document.getElementById('comodidade1');
+let comod2 = document.getElementById('comodidade2');
+
+/* importante fim */
+
+sun.addEventListener('click', () => {
+    header.classList.remove('dark');
+    menu.classList.remove('dark');
+    cog.classList.remove('dark');
+    aside.classList.remove('dark');
+    user.classList.remove('dark');
+    home.classList.remove('dark');
+    bell.classList.remove('dark');
+    date.classList.remove('dark');
+    cart.classList.remove('dark');
+    theme.classList.remove('dark');
+    moon.classList.remove('dark');
+    sun.classList.remove('dark');
+    angle.classList.remove('dark');
+    img1.classList.remove('dark');
+    img2.classList.remove('dark');
+    img3.classList.remove('dark');
+    config.classList.remove('dark');
+    perfil.classList.remove('dark');
+    casa.classList.remove('dark');
+    paravoce.classList.remove('dark');
+    agnd.classList.remove('dark');
+    package.classList.remove('dark');
+    troca.classList.remove('dark');
+    check.classList.remove('dark');
+    cabelo.classList.remove('dark');
+    barba.classList.remove('dark');
+    sombrancelha.classList.remove('dark');
+    comod1.classList.remove('dark');
+    comod2.classList.remove('dark');
+});
