04d2265e9c969e6525049da518379fa1cf5dc70e
Fábio Luiz <142447343+Fabio42-sys@users.noreply.github.com>
2024-02-18 01:22:59 +0000

    feat: criando sistema de arrays na pagina

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.getElementById('openMenu').addEventListener('click', function() {
+document.getElementById('openMenu').addEventListener('click', function () {
     let aside = document.getElementById('menuAside')
 
-    if (aside.classList.contains('menu-aside')){
+    if (aside.classList.contains('menu-aside')) {
         aside.classList.remove('menu-aside');
         aside.classList.add('menu-lateral');
     } else {
@@ -9,3 +9,15 @@ document.getElementById('openMenu').addEventListener('click', function() {
         aside.classList.add('menu-aside');
     }
 });
+
+// Função para carregar e exibir o JSON do arquivo accounts.json
+function carregarContas() {
+    fetch('barber.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log('Dados das contas:', data);
+        })
+        .catch(error => {
+            console.error('Ocorreu um erro ao carregar o arquivo JSON:', error);
+        });
+}
