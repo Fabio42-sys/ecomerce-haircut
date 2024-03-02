51e8fb5aac6043aec8129283ba3d6b52f92359fd
Fábio Luiz <142447343+Fabio42-sys@users.noreply.github.com>
2024-03-02 03:05:21 +0000

    feat: criação da pagina de login

---
+++ frontend/js/login.js
@@ -0,0 +1,77 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+
+// const btnPassword = document.getElementById('passwordInput');
+// const iconEye = document.getElementById('img');
+
+// iconEye.addEventListener('click', () => {
+//     if (btnPassword.type == 'password') {
+//         btnPassword.type = 'text';
+//         iconEye.src 
+//     }
+// });
+
+// iconEye.addEventListener('click', () => {
+
+//     let iconAntes = iconEye.src = "img/cadeado-fechado.svg";
+//     let iconDepois = iconEye.src = "img/cadeado-aberto.svg";
+
+//     if (btnPassword.type == 'password') {
+//         btnPassword.type = 'text';
+//         iconEye.scr == iconDepois;
+//     } else if (btnPassword.type == 'text') {
+//         btnPassword.type = 'password';
+//         iconEye.scr == iconAntes;
+//     }
+
+// });
+
+const showPass = document.getElementById('passwordLogin');
+const iconEye = document.getElementById('imgLogin');
+
+iconEye.addEventListener('click', event => {
+
+    if (event.target.classList.contains('cadeado')) {
+
+        let iconeAntes = "frontend/img/cadeado-fechado.svg";
+        let iconeDepois = "frontend/img/cadeado-aberto.svg";
+
+        if(event.target.src.includes(iconeAntes)) {
+            showPass.type = 'text';
+            event.target.src = iconeDepois
+        } else {
+            showPass.type = 'password';
+            event.target.src = iconeAntes
+        }
+    }
+});
+
+const password = document.getElementById('passwordCadastro');
+const cadeado = document.getElementById('imgCadastro');
+
+cadeado.addEventListener('click', event => {
+
+    if (event.target.classList.contains('cadeado')) {
+
+        let iconeAntes = "frontend/img/cadeado-fechado.svg";
+        let iconeDepois = "frontend/img/cadeado-aberto.svg";
+
+        if(event.target.src.includes(iconeAntes)) {
+            password.type = 'text';
+            event.target.src = iconeDepois
+        } else {
+            password.type = 'password';
+            event.target.src = iconeAntes
+        }
+    }
+});
